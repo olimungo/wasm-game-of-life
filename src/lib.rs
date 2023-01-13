435550689e8f0786fdc4da7ed14ba9fd96ed3e2c
Olivier Mungo <olivier@mungo.eu>
2023-01-13 08:55:43 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,117 @@
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+extern crate js_sys;
+extern crate fixedbitset;
+use fixedbitset::FixedBitSet;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+pub struct ChangedCell((u32, u32), bool);
+
+#[wasm_bindgen]
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: FixedBitSet,
+    changed_cells: Vec<ChangedCell>
+}
+
+/// Public methods, exported to JavaScript
+#[wasm_bindgen]
+impl Universe {
+    pub fn new(width: u32, height: u32) -> Universe {
+        let size = (width * height) as usize;
+        let mut cells = FixedBitSet::with_capacity(size);
+        // let changed_cells : Vec<ChangedCell>= Vec::new();
+        let changed_cells: Vec<ChangedCell> = vec![ChangedCell((6, 6), true)];
+    
+        // for i in 0..size {
+        //     cells.set(i, i % 2 == 0 || i % 7 == 0);
+        // }
+
+        for i in 0..size {
+            cells.set(i, if js_sys::Math::random() < 0.5 { true } else { false });
+        }
+    
+        Universe {
+            width,
+            height,
+            cells,
+            changed_cells,
+        }
+    }
+
+    pub fn tick(&mut self, iterations: u32) {
+        for _ in 0..iterations {
+            let mut new_cells = self.cells.clone();
+            let mut changed_cells : Vec<ChangedCell>= Vec::new();
+
+            for row in 0..self.height {
+                for col in 0..self.width {
+                    let index = self.get_index(row, col);
+                    let is_alive = self.cells[index];
+                    let live_neighbour_count = self.live_neighbour_count(row, col);
+
+                    new_cells.set(index, match (live_neighbour_count, is_alive) {
+                        (2, true) | (3, _) => true,
+                        _ => false
+                    });
+
+                    if is_alive != new_cells[index] {
+                        changed_cells.push(ChangedCell ((row, col), new_cells[index]));
+                    }
+                }
+            }
+
+            self.cells = new_cells;
+            self.changed_cells = changed_cells;
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const u32 {
+        self.cells.as_slice().as_ptr()
+    }
+
+    pub fn changed_cells(&self) -> *const ChangedCell {
+        self.changed_cells.as_slice().as_ptr()
+    }
+
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn live_neighbour_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+
+                count += self.cells[idx] as u8;
+            }
+        }
+
+        count
+    }
+}
---
+++ www/index.js
@@ -0,0 +1,135 @@
+import { Universe } from 'wasm-game-of-life';
+import { memory } from 'wasm-game-of-life/wasm_game_of_life_bg';
+
+const CELL_SIZE = 3; // px
+const GRID_COLOR = '#CCCCCC';
+const DEAD_COLOR = '#FFFFFF';
+const ALIVE_COLOR = '#000000';
+const MAX_TICK = 30000;
+const TICK_AT_ONCE = 1;
+
+let WIDTH = 100;
+let HEIGHT = 100;
+let universe;
+
+// Give the canvas room for all of our cells and a 1px border
+// around each of them.
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+
+// Construct the universe, and get its width and height.
+const createUniverse = () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    universe = Universe.new(WIDTH, HEIGHT);
+    canvas.height = (CELL_SIZE + 1) * HEIGHT + 1;
+    canvas.width = (CELL_SIZE + 1) * WIDTH + 1;
+
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, (WIDTH * HEIGHT) / 8);
+
+    console.log(cells);
+};
+
+const getIndex = (row, column) => {
+    return row * WIDTH + column;
+};
+
+const isAlive = (n, arr) => {
+    const byte = Math.floor(n / 8);
+    const mask = 1 << n % 8;
+    return (arr[byte] & mask) === mask;
+};
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, (WIDTH * HEIGHT) / 8);
+
+    context.beginPath();
+
+    for (let row = 0; row < HEIGHT; row++) {
+        for (let col = 0; col < WIDTH; col++) {
+            const index = getIndex(row, col);
+
+            context.fillStyle = isAlive(index, cells)
+                ? ALIVE_COLOR
+                : DEAD_COLOR;
+
+            context.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    context.stroke();
+};
+
+const drawGrid = () => {
+    context.beginPath();
+    context.strokeStyle = GRID_COLOR;
+
+    // Vertical lines.
+    for (let i = 0; i <= WIDTH; i++) {
+        context.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        context.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * HEIGHT + 1);
+    }
+
+    // Horizontal lines.
+    for (let j = 0; j <= HEIGHT; j++) {
+        context.moveTo(0, j * (CELL_SIZE + 1) + 1);
+        context.lineTo((CELL_SIZE + 1) * WIDTH + 1, j * (CELL_SIZE + 1) + 1);
+    }
+
+    context.stroke();
+};
+
+let tickCount = 0;
+
+const renderLoop = () => {
+    tickCount += TICK_AT_ONCE;
+
+    drawGrid();
+    drawCells();
+
+    const changedCellsPtr = universe.changed_cells();
+    // const changed_cells = new Uint8Array(memory.buffer, changedCellsPtr);
+
+    // console.log(changed_cells);
+    // const x = new ChangedCell();
+
+    // console.log(x);
+
+    if (tickCount <= MAX_TICK) {
+        requestAnimationFrame(renderLoop);
+    }
+
+    universe.tick(TICK_AT_ONCE);
+};
+
+const sliderWidth = document.getElementById('sliderWidth');
+const sliderWidthValue = document.getElementById('sliderWidthValue');
+const sliderHeight = document.getElementById('sliderHeight');
+const sliderHeightValue = document.getElementById('sliderHeightValue');
+
+sliderWidth.addEventListener('input', (e) => {
+    sliderWidthValue.textContent = e.target.value;
+});
+
+sliderHeight.addEventListener('input', (e) => {
+    sliderHeightValue.textContent = e.target.value;
+});
+
+sliderWidth.addEventListener('change', (e) => {
+    WIDTH = e.target.value;
+    createUniverse();
+});
+
+sliderHeight.addEventListener('change', (e) => {
+    HEIGHT = e.target.value;
+    createUniverse();
+});
+
+createUniverse();
+// requestAnimationFrame(renderLoop);
