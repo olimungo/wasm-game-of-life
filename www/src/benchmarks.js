71da458ae46d25a2dbf775416c5a7706b8f0c9ae
Olivier Mungo <1698539+olimungo@users.noreply.github.com>
2023-01-28 22:58:18 +0100

    Refactoring (#28)

---
+++ www/src/benchmarks.js
@@ -0,0 +1,89 @@
+export function Benchmarks() {
+    const uiBenchmarksPanel = document.getElementById('ui-benchmarks-panel');
+    const uiTotalDuration = document.getElementById('ui-total-duration');
+    const uiTotalTicksDuration = document.getElementById(
+        'ui-total-ticks-duration'
+    );
+    const uiAverageTickDuration = document.getElementById(
+        'ui-average-tick-duration'
+    );
+    const uiTotalRedrawDuration = document.getElementById(
+        'ui-total-redraw-duration'
+    );
+    const uiAverageRedrawDuration = document.getElementById(
+        'ui-average-redraw-duration'
+    );
+    const uiAutomaticOpenSwitch = document.getElementById(
+        'ui-automatic-open-switch'
+    );
+    const uiAutomaticOpenBallContainer = document.getElementById(
+        'ui-automatic-open-ball-container'
+    );
+    const uiAutomaticOpenBall = document.getElementById(
+        'ui-automatic-open-ball'
+    );
+    const uiAutomaticOpenLabel = document.getElementById(
+        'ui-automatic-open-label'
+    );
+
+    uiBenchmarksPanel.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        if (
+            event.target === uiAutomaticOpenBallContainer ||
+            event.target === uiAutomaticOpenBall ||
+            event.target === uiAutomaticOpenLabel
+        ) {
+            uiAutomaticOpenSwitch.checked = !uiAutomaticOpenSwitch.checked;
+        } else {
+            if (uiBenchmarksPanel.classList.contains('open')) {
+                uiBenchmarksPanel.classList.remove('open');
+            } else {
+                uiBenchmarksPanel.classList.add('open');
+            }
+        }
+    });
+
+    return {
+        openResults,
+        setResults,
+        resetResults,
+    };
+
+    //
+    // Functions
+    //
+
+    function openResults() {
+        if (uiAutomaticOpenSwitch.checked) {
+            if (!uiBenchmarksPanel.classList.contains('open')) {
+                uiBenchmarksPanel.classList.add('open');
+            }
+        }
+    }
+
+    function formatMilliseconds(element, value) {
+        element.textContent = (Math.round(value * 100) / 100).toFixed(2);
+    }
+
+    function setResults(results) {
+        formatMilliseconds(uiTotalDuration, results.totalDuration);
+        formatMilliseconds(uiTotalTicksDuration, results.totalTicksDuration);
+        formatMilliseconds(uiAverageTickDuration, results.averageTickDuration);
+        formatMilliseconds(uiTotalRedrawDuration, results.totalRedrawDuration);
+        formatMilliseconds(
+            uiAverageRedrawDuration,
+            results.averageRedrawDuration
+        );
+    }
+
+    function resetResults() {
+        setResults({
+            totalDuration: 0,
+            totalTicksDuration: 0,
+            averageTickDuration: 0,
+            totalRedrawDuration: 0,
+            averageRedrawDuration: 0,
+        });
+    }
+}
--- www/src/ui.js
+++ www/src/ui.js
@@ -1,12 +1,6 @@
-export const ColonyGenerationType = Object.freeze({
-    Pattern: Symbol('pattern'),
-    Randomly: Symbol('randomly'),
-});
-
-export const EngineGenerationType = Object.freeze({
-    Wasm: Symbol('wasm'),
-    Javascript: Symbol('javascript'),
-});
+import { Throttle } from './throttle';
+import { Benchmarks } from './benchmarks';
+import { Inputs } from './inputs';
 
 export function Ui(
     playClickedCallback,
@@ -14,44 +8,14 @@ export function Ui(
     resetClickedCalback,
     cellClickedCallback
 ) {
+    const throttle = Throttle();
+    const benchmarks = Benchmarks();
+    const inputs = Inputs(reset);
     const githubSourcesUrl = 'https://github.com/olimungo/wasm-game-of-life';
-
-    let cellSize; // px
-    let width; // px
-    let height; // px
-    let numberOfGenerations;
-    let ticksAtOnce;
-    let throttle; // ms
-    let colonyGenerationType;
-    let engineGenerationType;
 
     //
     // HTML elements reference
     //
-
-    // Inputs
-    const uiWidth = document.getElementById('ui-width');
-    const uiHeight = document.getElementById('ui-height');
-    const uiCounter = document.getElementById('ui-counter');
-    const uiCellSize = document.getElementById('ui-cell-size');
-    const uiNumberOfGenerations = document.getElementById(
-        'ui-number-of-generations'
-    );
-    const uiGenerationsAtOnce = document.getElementById(
-        'ui-generations-at-once'
-    );
-
-    // Throttle
-    const uiThrottle = document.getElementById('ui-throttle');
-    const uiThrottleValue = document.getElementById('ui-throttle-value');
-
-    // Radio buttons
-    const uiColonyGenerationTypes = document.querySelectorAll(
-        'input[name="ui-colony-generation-type"]'
-    );
-    const uiEngineGenerationTypes = document.querySelectorAll(
-        'input[name="ui-engine-generation-type"]'
-    );
 
     // Buttons
     const uiSet = document.getElementById('ui-set');
@@ -62,45 +26,16 @@ export function Ui(
     // Library
     const uiLibraryPanel = document.getElementById('ui-library-panel');
 
-    // Results
-    const uiResultsPanel = document.getElementById('ui-results-panel');
-    const uiTotalDuration = document.getElementById('ui-total-duration');
-    const uiTotalTicksDuration = document.getElementById(
-        'ui-total-ticks-duration'
-    );
-    const uiAverageTickDuration = document.getElementById(
-        'ui-average-tick-duration'
-    );
-    const uiTotalRedrawDuration = document.getElementById(
-        'ui-total-redraw-duration'
-    );
-    const uiAverageRedrawDuration = document.getElementById(
-        'ui-average-redraw-duration'
-    );
-    const uiAutomaticOpenSwitch = document.getElementById(
-        'ui-automatic-open-switch'
-    );
-    const uiAutomaticOpenBallContainer = document.getElementById(
-        'ui-automatic-open-ball-container'
-    );
-    const uiAutomaticOpenBall = document.getElementById(
-        'ui-automatic-open-ball'
-    );
-    const uiAutomaticOpenLabel = document.getElementById(
-        'ui-automatic-open-label'
-    );
-
     // Canvas
     const uiCanvas = document.getElementById('ui-canvas');
 
-    initProperties();
     addEventListeners();
 
     return {
         getWidth,
         getHeight,
         getCellSize,
-        getTicksAtOnce,
+        getGenerationsAtOnce,
         getThrottle,
         getColonyGenerationType,
         getEngineGenerationType,
@@ -113,77 +48,48 @@ export function Ui(
     };
 
     //
-    // Getters
+    // Getters/Setters
     //
 
     function getWidth() {
-        return width;
+        return inputs.getWidth();
     }
 
     function getHeight() {
-        return height;
+        return inputs.getHeight();
     }
 
-    function getTicksAtOnce() {
-        return ticksAtOnce;
+    function getGenerationsAtOnce() {
+        return inputs.getGenerationsAtOnce();
     }
 
     function getCellSize() {
-        return cellSize;
+        return inputs.getCellSize();
     }
 
     function getColonyGenerationType() {
-        return colonyGenerationType;
+        return inputs.getColonyGenerationType();
     }
 
     function getEngineGenerationType() {
-        return engineGenerationType;
+        return inputs.getEngineGenerationType();
     }
 
     function getThrottle() {
-        return throttle;
+        return throttle.getThrottle();
     }
 
     function getNumberOfGenerations() {
-        return numberOfGenerations;
+        return inputs.getNumberOfGenerations();
+    }
+
+    function setUiCounter(count) {
+        inputs.setUiCounter(count);
     }
 
     //
     // Functions
     //
-
-    function initProperties() {
-        for (let radioButton of uiEngineGenerationTypes) {
-            if (radioButton.checked) {
-                engineGenerationType =
-                    radioButton.value === 'wasm'
-                        ? EngineGenerationType.Wasm
-                        : EngineGenerationType.Javascript;
-            }
-        }
-
-        for (let radioButton of uiColonyGenerationTypes) {
-            if (radioButton.checked) {
-                colonyGenerationType =
-                    radioButton.value === 'pattern'
-                        ? ColonyGenerationType.Pattern
-                        : ColonyGenerationType.Randomly;
-            }
-        }
-
-        width = parseInt(uiWidth.value);
-        height = parseInt(uiHeight.value);
-        cellSize = parseInt(uiCellSize.value);
-        numberOfGenerations = parseInt(uiNumberOfGenerations.value);
-        ticksAtOnce = parseInt(uiGenerationsAtOnce.value);
-        throttle = parseInt(uiThrottleValue.textContent);
-
-        setUiCounter(0);
-    }
-
-    function setUiCounter(count) {
-        uiCounter.textContent = count;
-    }
 
     function setPlayButton() {
         uiPlayPause.textContent = 'PLAY';
@@ -194,81 +100,8 @@ export function Ui(
         resetClickedCalback();
     }
 
-    function unwrapDefault(value, defaultValue) {
-        const intValue = parseInt(value);
-
-        if (!isNaN(intValue)) {
-            return intValue;
-        }
-
-        return defaultValue;
-    }
-
     function addEventListeners() {
-        for (const radioButton of uiEngineGenerationTypes) {
-            radioButton.addEventListener('change', (e) => {
-                engineGenerationType =
-                    e.target.value === 'wasm'
-                        ? EngineGenerationType.Wasm
-                        : EngineGenerationType.Javascript;
-
-                reset();
-            });
-        }
-
-        uiNumberOfGenerations.addEventListener(
-            'change',
-            (e) =>
-                (numberOfGenerations = unwrapDefault(
-                    e.target.value,
-                    numberOfGenerations
-                ))
-        );
-
-        uiNumberOfGenerations.addEventListener(
-            'keyup',
-            (e) =>
-                (numberOfGenerations = unwrapDefault(
-                    e.target.value,
-                    numberOfGenerations
-                ))
-        );
-
-        uiGenerationsAtOnce.addEventListener(
-            'change',
-            (e) => (ticksAtOnce = unwrapDefault(e.target.value, ticksAtOnce))
-        );
-
-        uiGenerationsAtOnce.addEventListener(
-            'keyup',
-            (e) => (ticksAtOnce = unwrapDefault(e.target.value, ticksAtOnce))
-        );
-
-        uiThrottle.addEventListener('input', (event) => {
-            throttle = event.target.value;
-            uiThrottleValue.textContent = throttle;
-        });
-
-        for (const radioButton of uiColonyGenerationTypes) {
-            radioButton.addEventListener('change', (e) => {
-                colonyGenerationType =
-                    e.target.value === 'pattern'
-                        ? ColonyGenerationType.Pattern
-                        : ColonyGenerationType.Randomly;
-
-                reset();
-            });
-        }
-
-        function set() {
-            width = unwrapDefault(uiWidth.value, width);
-            height = unwrapDefault(uiHeight.value, height);
-            cellSize = unwrapDefault(uiCellSize.value, cellSize);
-
-            reset();
-        }
-
-        uiSet.addEventListener('click', set);
+        uiSet.addEventListener('click', reset);
         uiReset.addEventListener('click', reset);
 
         uiPlayPause.addEventListener('click', (e) => {
@@ -286,24 +119,6 @@ export function Ui(
                 uiLibraryPanel.classList.remove('open');
             } else {
                 uiLibraryPanel.classList.add('open');
-            }
-        });
-
-        uiResultsPanel.addEventListener('click', (event) => {
-            event.preventDefault();
-
-            if (
-                event.target === uiAutomaticOpenBallContainer ||
-                event.target === uiAutomaticOpenBall ||
-                event.target === uiAutomaticOpenLabel
-            ) {
-                uiAutomaticOpenSwitch.checked = !uiAutomaticOpenSwitch.checked;
-            } else {
-                if (uiResultsPanel.classList.contains('open')) {
-                    uiResultsPanel.classList.remove('open');
-                } else {
-                    uiResultsPanel.classList.add('open');
-                }
             }
         });
 
@@ -333,35 +148,14 @@ export function Ui(
     }
 
     function openResults() {
-        if (uiAutomaticOpenSwitch.checked) {
-            if (!uiResultsPanel.classList.contains('open')) {
-                uiResultsPanel.classList.add('open');
-            }
-        }
-    }
-
-    function formatMilliseconds(element, value) {
-        element.textContent = (Math.round(value * 100) / 100).toFixed(2);
+        benchmarks.openResults();
     }
 
     function setResults(results) {
-        formatMilliseconds(uiTotalDuration, results.totalDuration);
-        formatMilliseconds(uiTotalTicksDuration, results.totalTicksDuration);
-        formatMilliseconds(uiAverageTickDuration, results.averageTickDuration);
-        formatMilliseconds(uiTotalRedrawDuration, results.totalRedrawDuration);
-        formatMilliseconds(
-            uiAverageRedrawDuration,
-            results.averageRedrawDuration
-        );
+        benchmarks.setResults(results);
     }
 
     function resetResults() {
-        setResults({
-            totalDuration: 0,
-            totalTicksDuration: 0,
-            averageTickDuration: 0,
-            totalRedrawDuration: 0,
-            averageRedrawDuration: 0,
-        });
+        benchmarks.resetResults();
     }
 }
